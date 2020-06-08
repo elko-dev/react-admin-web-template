6541ba869e3e5e44a989f503855a1effe74c46c4
CodyTroopDev <cody.troop@elko.dev>
2020-06-08 15:33:53 -0500

    hooked user store up to user service

---
+++ src/service/UserService.ts
@@ -0,0 +1,119 @@
+import { ApiService } from './ApiService';
+import { User } from 'models/User';
+
+export class UserService {
+    private apiService = new ApiService();
+
+    public async signUpUser(email: string, password: string, firstName?: string, lastName?: string, phoneNumber?: string): Promise<User> {
+        const mutationString: string = `
+        mutation { signUpUser(
+            email: "${email}"
+            password: "${password}"
+            ${firstName ? `firstName: "${firstName}"` : ''}
+            ${lastName ? `lastName: "${lastName}"` : ''}
+            ${phoneNumber ? `phoneNumber: "${phoneNumber}"` : ''}
+          ) {
+                user {
+                    id
+                    email
+                    firstName
+                    lastName
+                    phoneNumber
+                    accounts{
+                        id
+                        name
+                        createDate
+                        updateDate
+                    }
+                }
+                errors {
+                    message
+                }
+            }
+        }
+      `;
+
+        // return request(BASE_API, mutationString).then((data: any) => {
+        //     if (data.signUpUser.user === null || data.signUpUser.errors.length > 0) {
+        //         throw (data.signUpUser.errors);
+        //     }
+        //     return new User(data.signUpUser.user);
+        // }
+        // );
+        try {
+            const response = await this.apiService.authenticatedGqlQuery(mutationString);
+            if (response.signUpUser.user === null || response.signUpUser.errors.length > 0) {
+                throw (response.signUpUser.errors);
+            }
+            return new User(response.signUpUser.user);
+        } catch (error) {
+            throw (error);
+        }
+    }
+
+    public async getAuthenticatedUser(authUser: firebase.User): Promise<User> {
+        const query: string = `
+        query { getUserByFirebaseId(
+            id: "${authUser.uid}"
+          ) {
+                user {
+                    id
+                    email
+                    firstName
+                    lastName
+                    phoneNumber
+                }
+                errors {
+                    message
+                }
+            }
+        }
+      `;
+
+        try {
+            const response = await this.apiService.authenticatedGqlQuery(query);
+            if (response.getUserByFirebaseId.user === null || response.getUserByFirebaseId.errors.length > 0) {
+                throw (response.getUserByFirebaseId.errors);
+            }
+            return new User(response.getUserByFirebaseId.user);
+        } catch (error) {
+            throw (error);
+        }
+    }
+
+    public async signUpAuthUser(email: string, firstName: string, lastName: string, authId: string,
+        phoneNumber: string | null): Promise<User> {
+        const mutationString: string = `
+        mutation { signUpAuthorizedUser(
+            email: "${email}"
+            firstName: "${firstName}"
+            lastName: "${lastName}"
+            phoneNumber: "${phoneNumber}"
+            authId: "${authId}"
+          ) {
+                user {
+                    id
+                    email
+                    firstName
+                    lastName
+                    phoneNumber
+                    authId
+                }
+                errors {
+                    message
+                }
+            }
+        }
+      `;
+
+        try {
+            const response = await this.apiService.authenticatedGqlQuery(mutationString);
+            if (response.signUpAuthorizedUser.user === null || response.signUpAuthorizedUser.errors.length > 0) {
+                throw (response.signUpAuthorizedUser.errors);
+            }
+            return new User(response.signUpAuthorizedUser.user);
+        } catch (error) {
+            throw (error);
+        }
+    }
+}
--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -1,5 +1,7 @@
-// import { observable, computed, action } from "mobx";
 import { User } from "../models/User";
+import { observable, computed, action } from 'mobx';
+import { UserService } from 'service/UserService';
+import { firebaseAuth } from 'config/auth.config';
 
 export interface IUserStore {
     _user: User | null;
@@ -10,37 +12,40 @@ export interface IUserStore {
 
 export class UserStore {
 
-    // constructor() {
-    //     this.listenUserAuth();
-    // }
+    constructor() {
+        this.listenUserAuth();
+    }
 
-    // @observable public _user: User | null = null;
+    private listenUserAuth = (): void => {
+        firebaseAuth.onAuthStateChanged((authUser) => {
+            if (authUser) {
+                this.getCurrentUser(authUser);
+            } else {
+                this.getCurrentUser(undefined);
+            }
+        });
+    }
 
-    // @computed
-    // public get user(): User | null {
-    //     return this._user;
-    // }
+    private userService: UserService = new UserService();
 
-    // @action
-    // public async getCurrentUser(firebaseId: string, authUser?: firebase.User): Promise<void> {
-    //     const existingUser: User | null = await UserService.getUserByFireBaseToken(firebaseId);
-    //     if ((existingUser === null) && authUser) {
-    //         const newUser: User | null = await UserService.addUserFromFirebaseUser(authUser);
-    //         this._user = newUser;
-    //     } else {
-    //         this._user = existingUser;
-    //     }
-    // }
+    @observable public _user: User | null = null;
 
-    // public updateUser = (authUser: firebase.User | null) => {
-    //     if (null !== authUser) {
-    //         this.getCurrentUser(authUser.uid, authUser);
-    //     } else {
-    //         this._user = null;
-    //     }
-    // };
+    @computed
+    public get user(): User | null {
+        return this._user;
+    }
 
-    // public listenUserAuth(): void {
-    //     AuthService.listenUserAuth(this.updateUser);
-    // }
+    @action
+    public async getCurrentUser(authUser?: firebase.User): Promise<void> {
+        let existingUser: User | null = null;
+        if (authUser) {
+            existingUser = await this.userService.getAuthenticatedUser(authUser);
+        }
+        if ((existingUser === null) && authUser) {
+            const newUser: User | null = await this.userService.signUpAuthUser(authUser.email!, authUser.displayName!, '', authUser.uid, authUser.phoneNumber);
+            this._user = newUser;
+        } else {
+            this._user = existingUser;
+        }
+    }
 }
